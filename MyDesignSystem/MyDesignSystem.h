bc6d0dfb7bda65fbdf921bf8891c68b4b402e2a3
l.valle <laurent@travelcar.com>
2019-12-16 14:43:25 +0100

    Initial Commit

---
+++ MyDesignSystem/MyDesignSystem.h
@@ -0,0 +1,19 @@
+//
+//  MyDesignSystem.h
+//  MyDesignSystem
+//
+//  Created by Laurent on 16/12/2019.
+//  Copyright © 2019 TravelCar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyDesignSystem.
+FOUNDATION_EXPORT double MyDesignSystemVersionNumber;
+
+//! Project version string for MyDesignSystem.
+FOUNDATION_EXPORT const unsigned char MyDesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyDesignSystem/PublicHeader.h>
+
+
